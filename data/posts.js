acbea7b68f98a7d9a7c2bbdc3060573806ef2098
CodeJewelPro <ducheinej@gmail.com>
2024-07-29 15:07:54 -0400

    commit

---
+++ data/posts.js
@@ -0,0 +1,5 @@
+const post = [
+    {id:1, userId:1, title: "First Post", content: "This is Saniyah's first post"},
+    {id:2, userID:2, title: "Wassup PPl", content:" Johny says Wassup PPl"}, 
+    {id: 3, userID:3, title:"Iyanna's Thoughts", content:"Iyanna shares her thoughts"}, 
+]; 
