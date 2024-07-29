8db17f065ba6660be21fe992ffaaeb1c55b85c2a
CodeJewelPro <ducheinej@gmail.com>
2024-07-29 14:45:52 -0400

    commit

---
+++ data/users.js
@@ -0,0 +1,7 @@
+const users = [
+    {id: 1, name: "Saniyah", username: "saniyah123", email: "Saniyah0513@gmail.com"},
+    {id:  2, name: "Johny", username:"johny1005", email: "johnyd@gmail.com"},
+    {id: 3, name: "Iyanna", username: "iyanna1220", email: "iyanna24@gmail.com"},
+];
+
+module.exports = users; 
