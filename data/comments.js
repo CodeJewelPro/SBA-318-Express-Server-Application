367009a0fe09bcbc5da804abf102c845af2ef560
CodeJewelPro <ducheinej@gmail.com>
2024-07-29 23:23:49 -0400

    commit all

---
+++ data/comments.js
@@ -0,0 +1,5 @@
+const comments = [
+    {id:1, postId:1, text: " Love this post Saniyah!"}, 
+    {id:2,  postId:2, text:"Welcome to the community Johny!"},
+    {id:3, postId:3, text: "Thanks for sharing your thoughts Iyanna!",}
+];
